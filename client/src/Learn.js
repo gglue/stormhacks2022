503193ed0e1a4b4a708acb00828c2ed18182020a
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2022-02-19 03:21:11 -0500

    added description for dot symbol

--- client/src/Learn.js
+++ client/src/Learn.js
@@ -1,13 +1,19 @@
 import './App.css';
-import chart from './images/morse-chart_grande.png';
+import chart from './images/morse-chart_grande2.png';
+import dot from './images/morse_dot.png';
 
 function Learn(){
 
     return (
         <div className="Learn">
             <div className="topHeader">
-                <h1>How do I use Morse Code?</h1>
-                <img src={chart} className="learn-chart" alt="chart"></img>
+                <h1 id="title">How do I use Morse Code?<br></br><small>.... --- .-- / -.. --- / .. / ..- ... . / -- --- .-. ... . / -.-. --- -.. . ..--..</small></h1>
+                <img src={chart} id="chart-img" alt="chart"></img>
+            </div>
+
+            <div className="description">
+                <img src={dot} id="dot-img" alt="dot"></img>
+                <h2 id="dot-content">This symbol represents a dot. A length of a dot consist of one unit.</h2>
             </div>
         </div>
     )
