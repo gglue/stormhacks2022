75c89accaec694f92f2b78e5efcaf655af7c887a
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2022-02-19 02:50:44 -0500

    added title and image
    changed index.js so that it renders Learn rather than App (temporarily)

--- client/src/Learn.js
+++ client/src/Learn.js
@@ -1,9 +1,14 @@
-
+import './App.css';
+import chart from './images/morse-chart_grande.png';
 
 function Learn(){
 
     return (
         <div className="Learn">
+            <div className="topHeader">
+                <h1>How do I use Morse Code?</h1>
+                <img src={chart} className="learn-chart" alt="chart"></img>
+            </div>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Going to use Learn as the main app component for now
+//import App from './App';
+import Learn from './Learn';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Learn />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
