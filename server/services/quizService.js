a7c37efb98ba436eea897d663c8b6b0f76a3f257
Anthony Huang <Huang.Anthony@outlook.com>
2022-02-20 05:10:17 -0500

    Added quiz stats

--- server/services/quizService.js
+++ server/services/quizService.js
@@ -1,9 +1,8 @@
-const jwt = require("jsonwebtoken")
 const Word = require("../models/word")
-const mongoose = require("mongoose")
+const Profile = require("../models/profile")
 
 module.exports.getQuiz = async (req, res) => {
-    console.log("Profile Request")
+    console.log("Get Quiz Words Request")
 
     try {
         const noun = await Word.find().where({type: "noun"}).limit(20)
@@ -12,6 +11,63 @@ module.exports.getQuiz = async (req, res) => {
         res.status(201).json({noun, verb, preposition})
     }
     catch(error){
+        console.log(error.message)
+        res.status(400).json({error: "An error has occured"})
+    }
+}
+
+module.exports.porfileQuizCorrectIncrement = async (req, res) => {
+    console.log("Adding 1 to the quiz")
+    
+    try {
+        let profile = await Profile.findOne({user: req.user.user})
+
+        if (profile) {
+            profile.correct = profile.correct + 1
+            await Profile.updateOne({user: req.user.user}, {correct: profile.correct})
+
+            res.status(201).json({working: true})
+        }
+    }
+    catch (error){
+        console.log(error.message)
+        res.status(400).json({error: "An error has occured"})
+    }
+}
+
+module.exports.porfileQuizIncorrectIncrement = async (req, res) => {
+    console.log("Adding 1 to the quiz")
+    
+    try {
+        let profile = await Profile.findOne({user: req.user.user})
+
+        if (profile) {
+            profile.incorrect = profile.incorrect + 1
+            await Profile.updateOne({user: req.user.user}, {incorrect: profile.incorrect})
+
+            res.status(201).json({working: true})
+        }
+    }
+    catch (error){
+        console.log(error.message)
+        res.status(400).json({error: "An error has occured"})
+    }
+}
+
+module.exports.porfileQuizQuizIncrement = async (req, res) => {
+    console.log("Adding 1 to the quiz")
+    
+    try {
+        let profile = await Profile.findOne({user: req.user.user})
+
+        if (profile) {
+            profile.quizzes_done = profile.quizzes_done + 1
+            await Profile.updateOne({user: req.user.user}, {quizzes_done: profile.quizzes_done})
+
+            res.status(201).json({working: true})
+        }
+    }
+    catch (error){
         console.log(error.message)
         res.status(400).json({error: "An error has occured"})
     }
